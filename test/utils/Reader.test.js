c77952d28309113d9d99bbcde4d8c96fe4b03529
Emilio <emilio17mc@gmail.com>
2022-04-26 13:02:34 -0500

    Paso 5: Refactor de la lectura de archivo (Reader.js) y Unit Test

---
+++ test/utils/Reader.test.js
@@ -0,0 +1,10 @@
+const { exportAllDeclaration } = require("@babel/types");
+const Reader = require("./../../lib/utils/Reader");
+
+describe("Tests for Reader", () => {
+  test("1) readJsonFile not toBeUndefined", () => {
+    const explorers = Reader.readJsonFile("./explorers.json");
+
+    expect(explorers).not.toBeUndefined();
+  });
+});
