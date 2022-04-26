e02f7bef9e457b4137575b51c868eabb51aeb4ca
Emilio <emilio17mc@gmail.com>
2022-04-26 15:56:35 -0500

    Paso 13: Formateo con ESLint

--- test/controllers/ExplorerController.test.js
+++ test/controllers/ExplorerController.test.js
@@ -1,22 +1,22 @@
 
-const ExplorerController = require('./../../lib/controllers/ExplorerController');
+const ExplorerController = require("./../../lib/controllers/ExplorerController");
 
 describe("Tests for ExplorerController", () => {
     test("1) ExplorerController retorna una lista de Explorers de la misión dada", () => {
         
-        expect(ExplorerController.getExplorersByMission('node')).not.toBeUndefined();
+        expect(ExplorerController.getExplorersByMission("node")).not.toBeUndefined();
 
     });
 
     test("2) ExplorerController retorna una lista con los githubUsername de la misión dada", () => {
 
-        expect(ExplorerController.getExplorersUsernamesByMission('node')).not.toBeUndefined();
+        expect(ExplorerController.getExplorersUsernamesByMission("node")).not.toBeUndefined();
 
     });
 
     test("3) ExplorerController retorna la cantidad de explorers en la misión dada", () => {
 
-        expect(ExplorerController.getExplorersAmountByMission('node')).toBe(10);
+        expect(ExplorerController.getExplorersAmountByMission("node")).toBe(10);
 
     });
 });
